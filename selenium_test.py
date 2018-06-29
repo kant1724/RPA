d96954b78dea69b4de85c3f04627256f17813bc8
kant1724 <sonseungbum@naver.com>
2018-06-29 15:56:00 +0900

    commit

--- selenium_test.py
+++ selenium_test.py
@@ -13,6 +13,7 @@ cap['requireWindowFocus'] = True
 cap['INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS'] = True
 driver = webdriver.Ie(capabilities=cap, executable_path='./files/drivers/IEDriverServer64.exe')
 driver.get("http://www.facebook.com")
+driver.maximize_window()
 elem = driver.find_element_by_id("email")
 elem.send_keys(user)
 elem = driver.find_element_by_id("pass")

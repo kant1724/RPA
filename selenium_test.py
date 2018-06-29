b2a8b0cdf9540b2eb01ddb2ada54fd6a23a745e5
kant1724 <sonseungbum@naver.com>
2018-06-29 15:50:46 +0900

    commit

--- selenium_test.py
+++ selenium_test.py
@@ -1,8 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 user = ""
 pwd = ""
-driver = webdriver.Chrome('./files/drivers/chromedriver.exe')
+#driver = webdriver.Chrome('./files/drivers/chromedriver.exe')
+cap = DesiredCapabilities.INTERNETEXPLORER
+cap['ignoreProtectedModeSettings'] = True
+cap['IntroduceInstabilityByIgnoringProtectedModeSettings'] = True
+cap['nativeEvents'] = True
+cap['ignoreZoomSetting'] = True
+cap['requireWindowFocus'] = True
+cap['INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS'] = True
+driver = webdriver.Ie(capabilities=cap, executable_path='./files/drivers/IEDriverServer64.exe')
 driver.get("http://www.facebook.com")
 elem = driver.find_element_by_id("email")
 elem.send_keys(user)

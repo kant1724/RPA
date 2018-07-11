1d95d51d4b5a3e2d03e9747ea8ca91d70822086e
kant1724 <sonseungbum@naver.com>
2018-07-11 09:23:27 +0900

    commit

--- selenium_test.py
+++ selenium_test.py
@@ -1,8 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-user = ""
-pwd = ""
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.support.ui import WebDriverWait
+from contextlib import contextmanager
+
+@contextmanager
+def wait_for_page_load(driver, timeout=30.0):
+    source = driver.page_source
+    yield
+    WebDriverWait(driver, timeout, ignored_exceptions=(WebDriverException,)).until(lambda d: source != d.page_source)
 #driver = webdriver.Chrome('./files/drivers/chromedriver.exe')
 cap = DesiredCapabilities.INTERNETEXPLORER
 cap['ignoreProtectedModeSettings'] = True
@@ -12,11 +19,9 @@ cap['ignoreZoomSetting'] = True
 cap['requireWindowFocus'] = True
 cap['INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS'] = True
 driver = webdriver.Ie(capabilities=cap, executable_path='./files/drivers/IEDriverServer64.exe')
-driver.get("http://www.facebook.com")
-driver.maximize_window()
-elem = driver.find_element_by_id("email")
-elem.send_keys(user)
-elem = driver.find_element_by_id("pass")
-elem.send_keys(pwd)
-elem.send_keys(Keys.RETURN)
-driver.close()
+with wait_for_page_load(driver):
+    driver.get('https://youtube.com/')
+with wait_for_page_load(driver):
+    driver.get('https://github.com/')
+
+driver.quit()
